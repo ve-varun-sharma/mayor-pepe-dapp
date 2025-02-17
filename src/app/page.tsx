97bea448419f50e5b380b7703873816429d4d4e7
Varun Sharma <62218708+ve-varun-sharma@users.noreply.github.com>
2025-02-16 22:58:45 -0500

    fix build issue with spline upgrade next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-// import Spline from "@splinetool/react-spline/next";
+import Spline from "@splinetool/react-spline/next";
 import { cn } from "@/lib/utils";
 import { SparklesText } from "@/components/magicui/sparkles-text";
 import { HyperText } from "@/components/magicui/hyper-text";
@@ -16,9 +16,9 @@ export default function Home() {
             Mayor Pepe Token
           </HyperText>
           <SparklesText className="text-amber-500" text="$MAYOR" />
-          {/* <div className="aspect-square w-3/4 mx-auto relative rounded-2xl overflow-hidden">
+          <div className="aspect-square w-3/4 mx-auto relative rounded-2xl overflow-hidden">
             <Spline scene="https://prod.spline.design/ZSB-xEOdeUNPtdtE/scene.splinecode" />
-          </div> */}
+          </div>
           <div className="flex flex-row gap-4 justify-center">
             <Link href="/mint-nft">
               <InteractiveHoverButton>Mint Mayor NFT</InteractiveHoverButton>
